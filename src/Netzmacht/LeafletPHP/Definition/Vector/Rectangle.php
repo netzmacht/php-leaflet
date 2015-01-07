fb646117bdbe801e5a3bee31b138d0cc35cecfb3
David Molineus <david.molineus@netzmacht.de>
2015-01-07 18:58:50 +0100

    Add support for rectangles.

--- src/Netzmacht/LeafletPHP/Definition/Vector/Rectangle.php
+++ src/Netzmacht/LeafletPHP/Definition/Vector/Rectangle.php
@@ -11,12 +11,18 @@
 
 namespace Netzmacht\LeafletPHP\Definition\Vector;
 
+use Netzmacht\LeafletPHP\Definition\GeoJson\ConvertsToGeoJson;
+use Netzmacht\LeafletPHP\Definition\GeoJson\Feature;
+use Netzmacht\LeafletPHP\Definition\GeoJson\FeatureCollection;
+use Netzmacht\LeafletPHP\Definition\GeoJson\Geometry;
+use Netzmacht\LeafletPHP\Definition\Type\LatLngBounds;
+
 /**
  * Class Rectangle describes an rectangle on the map.
  *
  * @package Netzmacht\LeafletPHP\Definition\Vector
  */
-class Rectangle extends Polygon
+class Rectangle extends Path implements ConvertsToGeoJson, Geometry
 {
     /**
      * {@inheritdoc}
@@ -24,5 +30,77 @@ class Rectangle extends Polygon
     public static function getType()
     {
         return 'Rectangle';
+    }
+
+    /**
+     * LatLng bounds which defines the rectangle.
+     *
+     * @var LatLngBounds
+     */
+    private $bounds;
+
+    /**
+     * Construct.
+     *
+     * @param string       $identifier   The identifier.
+     * @param LatLngBounds $latLngBounds The bounds which defines the rectangle.
+     */
+    public function __construct($identifier, LatLngBounds $latLngBounds)
+    {
+        parent::__construct($identifier);
+
+        $this->bounds = $latLngBounds;
+    }
+
+    /**
+     * Get the bounds.
+     *
+     * @return LatLngBounds
+     */
+    public function getBounds()
+    {
+        return $this->bounds;
+    }
+
+    /**
+     * Get definition as feature collection.
+     *
+     * @return Feature|FeatureCollection
+     */
+    public function jsonSerialize()
+    {
+        $bounds = $this->getBounds();
+
+        // No rectangle support of geojson, so create a polygon
+        return array(
+            'type'        => 'Polygon',
+            'coordinates' => array(
+                array(
+                    $bounds->getNorthEast()->toGeoJson(),
+                    $bounds->getNorthWest()->toGeoJson(),
+                    $bounds->getSouthWest()->toGeoJson(),
+                    $bounds->getSouthEast()->toGeoJson()
+                )
+            )
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function toGeoJson()
+    {
+        $feature = new Feature($this, $this->getId());
+        $feature->setProperty('options', $this->getOptions());
+
+        if ($this->getPopup()) {
+            $feature->setProperty('popup', $this->getPopup());
+        }
+
+        if ($this->getPopupContent()) {
+            $feature->setProperty('popupContent', $this->getPopupContent());
+        }
+
+        return $feature;
     }
 }
