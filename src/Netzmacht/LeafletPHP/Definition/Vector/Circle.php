e68fee847b6c32e232670e6fc6f5cc8daebe44fb
David Molineus <david.molineus@netzmacht.de>
2015-07-24 16:15:16 +0200

    Circle inherit from CircleMarker now.

--- src/Netzmacht/LeafletPHP/Definition/Vector/Circle.php
+++ src/Netzmacht/LeafletPHP/Definition/Vector/Circle.php
@@ -12,15 +12,13 @@
 namespace Netzmacht\LeafletPHP\Definition\Vector;
 
 use Netzmacht\LeafletPHP\Value\GeoJson\FeatureTrait;
-use Netzmacht\LeafletPHP\Value\GeoJson\Geometry;
-use Netzmacht\LeafletPHP\Value\LatLng;
 
 /**
  * Class Circle represents a circle object on the map.
  *
  * @package Netzmacht\LeafletPHP\Definition\Vector
  */
-class Circle extends Path implements Geometry
+class Circle extends CircleMarker
 {
     use FeatureTrait;
 
@@ -30,89 +28,5 @@ class Circle extends Path implements Geometry
     public static function getType()
     {
         return 'Circle';
-    }
-
-    /**
-     * The lat lng.
-     *
-     * @var LatLng
-     */
-    private $latLng = array();
-
-    /**
-     * The radius in meters.
-     *
-     * @var int
-     */
-    private $radius = 0;
-
-    /**
-     * Get the lat lang.
-     *
-     * @return LatLng|null
-     */
-    public function getLatLng()
-    {
-        return $this->latLng;
-    }
-
-    /**
-     * Set the lat lang position.
-     *
-     * @param LatLng $latLng The coordinate.
-     *
-     * @return $this
-     */
-    public function setLatLng(LatLng $latLng)
-    {
-        $this->latLng = $latLng;
-
-        return $this;
-    }
-
-    /**
-     * Get the radius.
-     *
-     * @return int
-     */
-    public function getRadius()
-    {
-        return $this->radius;
-    }
-
-    /**
-     * Set the radius.
-     *
-     * @param int $radius The radius in meters.
-     *
-     * @return $this
-     */
-    public function setRadius($radius)
-    {
-        $this->radius = (int) $radius;
-
-        return $this;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function jsonSerialize()
-    {
-        return array(
-            'type'        => 'Point',
-            'coordinates' => $this->getLatLng()->toGeoJson()
-        );
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function toGeoJsonFeature()
-    {
-        $feature = $this->createFeature();
-        $feature->setProperty('radius', $this->getRadius());
-
-        return $feature;
     }
 }
--- src/Netzmacht/LeafletPHP/Definition/Vector/CircleMarker.php
+++ src/Netzmacht/LeafletPHP/Definition/Vector/CircleMarker.php
@@ -11,12 +11,15 @@
 
 namespace Netzmacht\LeafletPHP\Definition\Vector;
 
+use Netzmacht\LeafletPHP\Value\GeoJson\Geometry;
+use Netzmacht\LeafletPHP\Value\LatLng;
+
 /**
  * Class CircleMarker represents a circle marker object.
  *
  * @package Netzmacht\LeafletPHP\Definition\Vector
  */
-class CircleMarker extends Circle
+class CircleMarker extends Path implements Geometry
 {
     /**
      * {@inheritdoc}
@@ -24,5 +27,89 @@ class CircleMarker extends Circle
     public static function getType()
     {
         return 'CircleMarker';
+    }
+
+    /**
+     * The lat lng.
+     *
+     * @var LatLng
+     */
+    private $latLng = array();
+
+    /**
+     * The radius in meters.
+     *
+     * @var int
+     */
+    private $radius = 0;
+
+    /**
+     * Get the lat lang.
+     *
+     * @return LatLng|null
+     */
+    public function getLatLng()
+    {
+        return $this->latLng;
+    }
+
+    /**
+     * Set the lat lang position.
+     *
+     * @param LatLng $latLng The coordinate.
+     *
+     * @return $this
+     */
+    public function setLatLng(LatLng $latLng)
+    {
+        $this->latLng = $latLng;
+
+        return $this;
+    }
+
+    /**
+     * Get the radius.
+     *
+     * @return int
+     */
+    public function getRadius()
+    {
+        return $this->radius;
+    }
+
+    /**
+     * Set the radius.
+     *
+     * @param int $radius The radius in meters.
+     *
+     * @return $this
+     */
+    public function setRadius($radius)
+    {
+        $this->radius = (int) $radius;
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function jsonSerialize()
+    {
+        return array(
+            'type'        => 'Point',
+            'coordinates' => $this->getLatLng()->toGeoJson()
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function toGeoJsonFeature()
+    {
+        $feature = $this->createFeature();
+        $feature->setProperty('radius', $this->getRadius());
+
+        return $feature;
     }
 }
