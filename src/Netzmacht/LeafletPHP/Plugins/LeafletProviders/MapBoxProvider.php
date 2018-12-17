d84d94e82536e3d3b9799cd81fafad0d2a85f5f5
David Molineus <david.molineus@netzmacht.de>
2018-12-17 15:43:38 +0100

    Update copyright and license information. Add license file.

--- src/Netzmacht/LeafletPHP/Plugins/LeafletProviders/MapBoxProvider.php
+++ src/Netzmacht/LeafletPHP/Plugins/LeafletProviders/MapBoxProvider.php
@@ -6,7 +6,7 @@
  * @package    php-leaflet
  * @author     David Molineus <david.molineus@netzmacht.de>
  * @copyright  2014-2018 netzmacht David Molineus
- * @license    LGPL-3.0-or-later
+ * @license    LGPL-3.0-or-later https://github.com/netzmacht/php-leaflet/blob/master/LICENSE
  * @filesource
  */
 
