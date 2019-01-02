3b979dad2bb00c941431f7ba13cd38dad251da77
David Molineus <david.molineus@netzmacht.de>
2019-01-02 13:38:42 +0100

    Provide base test structure.

---
+++ tests/TestCase.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Netzmacht\LeafletPHP\Test;
+
+use Netzmacht\JavascriptBuilder\Builder;
+use Netzmacht\JavascriptBuilder\Encoder\ChainEncoder;
+use Netzmacht\JavascriptBuilder\Encoder\JavascriptEncoder;
+use Netzmacht\JavascriptBuilder\Encoder\MultipleObjectsEncoder;
+use Netzmacht\JavascriptBuilder\Output;
+use Netzmacht\JavascriptBuilder\Symfony\EventDispatchingEncoder;
+use Netzmacht\LeafletPHP\Encoder\ControlEncoder;
+use Netzmacht\LeafletPHP\Encoder\GroupEncoder;
+use Netzmacht\LeafletPHP\Encoder\MapEncoder;
+use Netzmacht\LeafletPHP\Encoder\RasterEncoder;
+use Netzmacht\LeafletPHP\Encoder\TypeEncoder;
+use Netzmacht\LeafletPHP\Encoder\UIEncoder;
+use Netzmacht\LeafletPHP\Encoder\VectorEncoder;
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+
+class TestCase extends PHPUnitTestCase
+{
+    protected function getBuilder()
+    {
+        $dispatcher = new EventDispatcher();
+
+        $dispatcher->addSubscriber(new ControlEncoder());
+        $dispatcher->addSubscriber(new GroupEncoder());
+        $dispatcher->addSubscriber(new MapEncoder());
+        $dispatcher->addSubscriber(new RasterEncoder());
+        $dispatcher->addSubscriber(new TypeEncoder());
+        $dispatcher->addSubscriber(new UIEncoder());
+        $dispatcher->addSubscriber(new VectorEncoder());
+
+        $factory = function (Output $output) use ($dispatcher) {
+            $encoder = new ChainEncoder();
+
+            $encoder->register(new MultipleObjectsEncoder());
+            $encoder->register(new EventDispatchingEncoder($dispatcher));
+            $encoder->register(new JavascriptEncoder($output));
+
+            return $encoder;
+        };
+
+        return new Builder($factory);
+    }
+}
